504e59eed6630645e07c466e5c28a93782814b7e
Alejandro Arco <alejandroarpu@gmail.com>
2025-03-02 17:44:35 +0100

    feat: add mostly all the logic from backend and also refactoring in frontend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ const fastify = require("fastify")({
   logger: true,
 });
 
+fastify.register(require("@fastify/cors"), {
+  origin: "http://localhost:8080",
+  methods: ["GET"],
+});
+
 fastify.get("/health", function (request, reply) {
   reply.send({ hello: "world" });
 });
@@ -19,18 +24,14 @@ const schema = {
 };
 
 const fetchTimeZone = async (place) => {
-  try {
-    const responseWeather = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}/2024-01-01?key=${process.env.VISUAL_CROSSING_API_KEY}&unitGroup=uk&timezone`
-    );
-    if (!responseWeather.ok) {
-      throw new Error("HTTP visual crossing error!");
-    }
-    const { timezone } = await responseWeather?.json();
-    return timezone;
-  } catch (error) {
-    console.log(`Error: ${error}`);
+  const responseWeather = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}/2025-01-01?key=${process.env.VISUAL_CROSSING_API_KEY}&unitGroup=uk&timezone`
+  );
+  if (!responseWeather.ok) {
+    throw new Error("HTTP visual crossing error!");
   }
+  const { timezone } = await responseWeather.json();
+  return timezone;
 };
 
 const getDateTimeZone = (timezone, offset = 0) => {
@@ -48,16 +49,21 @@ const getDateTimeZone = (timezone, offset = 0) => {
 fastify.get("/api/weather/:city", schema, async function (request, reply) {
   try {
     const { city } = request.params;
-    const responseWeather = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/2024-01-01?key=${process.env.VISUAL_CROSSING_API_KEY}&unitGroup=uk&timezone`
+    const placeTimezone = await fetchTimeZone(city);
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/${getDateTimeZone(
+        placeTimezone
+      )}/${getDateTimeZone(placeTimezone, 4)}/?key=${
+        process.env.VISUAL_CROSSING_API_KEY
+      }&unitGroup=uk`
     );
-    if (!responseWeather.ok) {
+    if (!response.ok) {
       const error = new Error("HTTP visual crossing error!");
-      error.code = responseWeather.status;
+      error.code = response.status;
       throw error;
     }
-    const data = await responseWeather.json();
-    reply.code(responseWeather.status).send(data);
+    const data = await response.json();
+    reply.code(response.status).send(data);
   } catch (error) {
     reply.code(error.code).send(error);
   }
